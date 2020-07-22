3c87d78f2a9f3ceb0ed0e977fe0065c6e514f62f
0utfire <nataniel.rr@gmail.com>
2020-07-22 13:19:44 +0300

    JS4 - done, except for Additional task

--- JS4/main.js
+++ JS4/main.js
@@ -152,8 +152,7 @@ function sumOfArrays(array1, array2) {
     return array3
 }
 
-console.log(sumOfArrays([1, 2, 3], [3, 2, 1])
-);
+console.log(sumOfArrays([1, 2, 3], [3, 2, 1]));
 
 // - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
 
@@ -167,33 +166,120 @@ swapI([1,2,3,4,5,6,7,'j','k'], 6)
 
 
 // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
-
-
-
 // Двожина масиву від 2 до 100
 // Приклад
 // [1,0,6,0,3] => [1,6,3,0,0]
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
 
+// let arr = [1,0,6,0,3];
+//
+// function moveZero(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i]===0) {
+//             array.splice(i, 1);
+//             array.push(0);
+//         }
+//     }
+// }
+// moveZero(arr);
+// console.log(arr);
+
 // Створити функцію яка :
-//     - Додає в боді блок з текстом "Hello owu"
+// - Додає в боді блок з текстом "Hello owu"
+
+function text() {
+    let div = document.createElement('div');
+    div.innerText = "Hello owu";
+    document.body.appendChild(div)
+}
+
 // - Додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
+
+// function addElem(element, text) {
+//     let elem = document.createElement(element);
+//     elem.innerText = text;
+//     document.body.appendChild(elem)
+// }
+// addElem('p', 'test text')
+
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-//     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
+//   Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
+
+// let cars = [
+//     car1 = {model: 'tesla', year: 2019, power: 400, color: 'black'},
+//     car2 = {model: 'fait', year: 2016, power: 140, color: 'blue'},
+//     car3 = {model: 'WV', year: 2013, power: 450, color: 'grey'},
+//     car4 = {model: 'Suzuki', year: 2011, power: 200, color: 'brown'},
+//     car5 = {model: 'Toyota', year: 2010, power: 500, color: 'yellow'},
+//     car6 = {model: 'Nissan', year: 2020, power: 100, color: 'red'},
+//     car7 = {model: 'Mazda', year: 2015, power: 120, color: 'purple'},
+//     car8 = {model: 'SEAT', year: 2018, power: 800, color: 'white'},
+//     car9 = {model: 'Porsche', year: 2019, power: 430, color: 'cream'},
+//     car10 = {model: 'Infiniti', year: 2012, power: 480, color: 'green'}
+// ];
+//
+// function carsFunc(array, id) {
+//     let elem = document.getElementById(id);
+//     for (const arrayElement of array) {
+//         let div = document.createElement('div');
+//         div.innerText = JSON.stringify(arrayElement);
+//         elem.appendChild(div);
+//     }
+// }
+// carsFunc(cars, 'const');
+
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-//     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
-//     Для кожної властивості створити всередені блока автомоблія свій блок
+//   Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
+//   Для кожної властивості створити всередені блока автомоблія свій блок
+
+// function carsFunc2(array, id) {
+//     let elem = document.getElementById(id);
+//     for (const arr of array) {
+//         let div = document.createElement('div');
+//         for (const item in arr) {
+//             let div2 = document.createElement('div');
+//             div2.innerText = `${item}: ${arr[item]}`;
+//             div.appendChild(div2)
+//         }
+//         elem.appendChild(div);
+//     }
+// }
+// carsFunc2(cars, 'const');
+
 //
 // (на основі минулого ДЗ)
 // **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
 // та повертає масив цих з'єднаних об'єктів.
 //     Приклад масивів:
-//     let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
-// let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false},
+];
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'},
+];
 // Частковий приклад реультату:
 //
 //
+function users(user, address) {
+    for (let i = 0; i < user.length; i++) {
+        for (let j = 0; j < address.length; j++) {
+            if (user[i].id === address[j].user_id) {
+                user[i].address = address[j]
+            }
+        }
+    }
+}
+users(usersWithId,citiesWithId);
+console.log(usersWithId);
+
+
 //
 //     ***- беремо завдання з правилами з укроку №3 :
 // Та робимо це функцією.При цьому правила отримувати через аргумент.
@@ -201,41 +287,55 @@ swapI([1,2,3,4,5,6,7,'j','k'], 6)
 // При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
 //     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
 //
-// let rules = [
-//     {
-//         title: 'Первое правило Бойцовского клуба.',
-//         body: 'Никому не рассказывать о Бойцовском клубе.'
-//     },
-//     {
-//         title: 'Второе правило Бойцовского клуба.',
-//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-//     },
-//     {
-//         title: 'Третье правило Бойцовского клуба.',
-//         body: 'В схватке участвуют только двое.'
-//     },
-//     {
-//         title: 'Четвертое правило Бойцовского клуба.',
-//         body: 'Не более одного поединка за один раз.'
-//     },
-//     {
-//         title: 'Пятое правило Бойцовского клуба.',
-//         body: 'Бойцы сражаются без обуви и голые по пояс.'
-//     },
-//     {
-//         title: 'Шестое правило Бойцовского клуба.',
-//         body: 'Поединок продолжается столько, сколько потребуется.'
-//     },
-//     {
-//         title: 'Седьмое правило Бойцовского клуба.',
-//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-//     },
-//     {
-//         title: 'Восьмое и последнее правило Бойцовского клуба.',
-//         body: 'Новичок обязан принять бой.'
-//     },
-//
-// ];
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
+
+];
+function rule(array) {
+    for (let i = 0; i < array.length; i++) {
+        let div = document.createElement('div');
+        let ruleNum = document.createElement('h2');
+        ruleNum.innerText = array[i].title;
+        div.appendChild(ruleNum);
+        let ruleTxt = document.createElement('p');
+        ruleTxt.innerText = array[i].body;
+        div.appendChild(ruleTxt);
+        document.body.appendChild(div)
+    }
+}
+rule(rules)
+
 // "
 //
 // ===========додаткове від віктора========
