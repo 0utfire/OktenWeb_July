bdc1f2c3873fdb486d4915927891166785b3f14e
0utfire <nataniel.rr@gmail.com>
2020-08-27 16:08:46 +0300

    Vue TASK 1

---
+++ vueFrame/myfirstvueapp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="text" v-model="todo">
+    <button @click="addToDO" v-if="items.length !== 10">Add new ToDo</button>
+    <ul v-for="(item, i) in items" :key= 'i'>
+      <li>{{item}}</li>
+      <button @click="removeToDo(i)">Remove</button>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      items: [],
+      todo: '',
+    }
+  },
+  methods: {
+    addToDO() {
+      this.items.push(this.todo);
+      this.todo= '';
+      console.log(this.items);
+    },
+    removeToDo(i){
+      this.items.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
